28220b5a8b52b5fd764736078f2d3d7fe637bb69
GamzeYaman <71887367+GamzeYaman@users.noreply.github.com>
2025-04-23 15:28:33 +0300

    SyncDebtConsumer first commit

---
+++ SyncDebtConsumer/src/main/java/com/app/consumer/SyncDebtConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.app.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncDebtConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncDebtConsumerApplication.class, args);
+	}
+
+}
